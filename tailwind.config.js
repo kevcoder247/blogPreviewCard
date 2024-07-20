581154916b5ce9a0347947b817c64b273b469abe
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2024-07-20 17:46:05 -0400

    wip add image and responsive styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./*html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      fontFamily: {
+        figtree: ["Figtree", "sans-serif"]
+      },
+      colors: {
+        yellow: 'hsl(47, 88%, 63%)',
+        white: 'hsl(0, 0%, 100%)',
+      },
+    },
   },
   plugins: [],
 }
